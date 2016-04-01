d70ba8cde1dc47de4c62b348c19eb9a43cedc0e8
Kai Kang <cctv2206@outlook.com>
2016-04-01 19:20:00 -0400

    Use a array to easily choose features

--- multisvm_driver_data.m
+++ multisvm_driver_data.m
@@ -1,16 +1,16 @@
 %% read data
-clc,close all, clear
+clc,close all, clear all
 
-driver1 = csvread('driver1.txt');
-driver2 = csvread('driver2.txt');
-driver3 = csvread('driver3.txt');
-driver4 = csvread('driver4.txt');
-driver5 = csvread('driver5.txt');
-driver6 = csvread('driver6.txt');
-driver7 = csvread('driver7.txt');
-driver8 = csvread('driver8.txt');
-driver9 = csvread('driver9.txt');
-driver10 = csvread('driver10.txt');
+driver1 = csvread('driver1_speed.txt');
+driver2 = csvread('driver2_speed.txt');
+driver3 = csvread('driver3_speed.txt');
+driver4 = csvread('driver4_speed.txt');
+driver5 = csvread('driver5_speed.txt');
+driver6 = csvread('driver6_speed.txt');
+driver7 = csvread('driver7_speed.txt');
+driver8 = csvread('driver8_speed.txt');
+driver9 = csvread('driver9_speed.txt');
+driver10 = csvread('driver10_speed.txt');
 
 
 %% multi class svm
@@ -26,9 +26,71 @@ size8 = length(driver8);
 size9 = length(driver9);
 size10 = length(driver10);
 
+%TrainingSet = [driver1 driver2 driver3 driver4 driver5 driver6 driver7 driver8 driver9 driver10]';
+%target = [ones(1, size1) 2*ones(1,size2) 3*ones(1, size3) 4*ones(1, size4) 5*ones(1, size5) 6*ones(1, size6) 7*ones(1, size7) 8*ones(1, size8) 9*ones(1, size9) 10*ones(1, size10)];
 
-TrainingSet = [driver1 driver2 driver3 driver4]';
-target = [1*ones(1, size1) 2*ones(1,size2) 3*ones(1,size3) 4*ones(1, size4)];
+
+% choose features here
+rowNums = [2 3 4 5 6 8];
+rows = length(rowNums);
+
+driver1_refined = [];
+for i = 1:rows
+    driver1_refined = [driver1_refined; driver1(i,:)];
+end
+
+driver2_refined = [];
+for i = 1:rows
+    driver2_refined = [driver2_refined; driver2(i,:)];
+end
+
+driver3_refined = [];
+for i = 1:rows
+    driver3_refined = [driver3_refined; driver3(i,:)];
+end
+
+driver4_refined = [];
+for i = 1:rows
+    driver4_refined = [driver4_refined; driver4(i,:)];
+end
+
+driver5_refined = [];
+for i = 1:rows
+    driver5_refined = [driver5_refined; driver5(i,:)];
+end
+
+driver6_refined = [];
+for i = 1:rows
+    driver6_refined = [driver6_refined; driver6(i,:)];
+end
+
+driver7_refined = [];
+for i = 1:rows
+    driver7_refined = [driver7_refined; driver7(i,:)];
+end
+
+driver8_refined = [];
+for i = 1:rows
+    driver8_refined = [driver8_refined; driver8(i,:)];
+end
+
+driver9_refined = [];
+for i = 1:rows
+    driver9_refined = [driver9_refined; driver9(i,:)];
+end
+
+driver10_refined = [];
+for i = 1:rows
+    driver10_refined = [driver10_refined; driver10(i,:)];
+end
+
+
+% TrainingSet = [driver1_refined driver2_refined driver3_refined driver4_refined]';
+% target = [ones(1, size1) 2*ones(1,size2) 3*ones(1, size3) 4*ones(1, size4)];
+
+TrainingSet = [driver1_refined driver2_refined driver3_refined driver4_refined driver5_refined driver6_refined driver7_refined driver8_refined driver9_refined driver10_refined]';
+target = [ones(1, size1) 2*ones(1,size2) 3*ones(1, size3) 4*ones(1, size4) 5*ones(1, size5) 6*ones(1, size6) 7*ones(1, size7) 8*ones(1, size8) 9*ones(1, size9) 10*ones(1, size10)];
+
 
 
 %% train and test using for loop
@@ -59,6 +121,7 @@ correct = count / tot_num
 
 compare = [target; predict];
 
-%%
+
+
 
 
